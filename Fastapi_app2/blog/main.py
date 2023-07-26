0aa0baf0a667dce30197a2d9476f2f1e36c5ad60
amankumar739728 <aman.kumar739728@gmail.com>
2023-07-26 12:28:29 +0530

    put command is updated
    put command updated

--- Fastapi_app2/blog/main.py
+++ Fastapi_app2/blog/main.py
@@ -36,12 +36,12 @@ def destroy(id,db:Session=Depends(get_db)):
     return JSONResponse(content={"response": "Delete successfully done"},status_code=status.HTTP_200_OK)
 
 
-@app.put('/blog/update/{id}',status_code=status.HTTP_202_ACCEPTED)
+@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
 def update_blog(id,request:schemas.Blog,db:Session =Depends(get_db)):
     blog=db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with id {id} not found")
-    blog.update(request)
+    blog.update(dict(request))
     db.commit()
     return JSONResponse(content={"response": "Blog Updated Successfully"},status_code=status.HTTP_200_OK)
 
